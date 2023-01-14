722cb9928333a01f52835c970453b987c0077a93
AhmadWildanAR <wildangokil1@gmail.com>
2023-01-14 14:48:49 +0800

    Merapikan Footer, Nav dan Sidebar

--- partials/footer.php
+++ partials/footer.php
@@ -1,9 +1,6 @@
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 0.0.1
     </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2023 <a href="#">Muhammad Wildan Abdurahman</a>.</strong> All rights reserved.
   </footer>
