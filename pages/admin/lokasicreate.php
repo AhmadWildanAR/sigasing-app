223bb4cc6d91cf77b7dde1db7b31a521d03e5f67
AhmadWildanAR <wildangokil1@gmail.com>
2023-01-15 21:20:53 +0800

    Membuat Perintah Create Data

--- pages/admin/lokasicreate.php
+++ pages/admin/lokasicreate.php
@@ -1,3 +1,39 @@
+<?php include_once "partials/scripts.php" ?>
+
+<?php
+if (isset($_POST['button_create'])) {
+	
+	$database = new Database();
+	$db       = $database->getConnection();
+
+	$validateSql = "SELECT * FROM lokasi WHERE nama_lokasi = ?";
+	$stmt 	   = $db->prepare($validatetSql);
+	$stmt->bindParam(1, $_POST['nama_lokasi']);
+	$stmt->execute();
+	if ($stmt->rowCount() > 0){
+?>
+    <div class="alert alert-danger alert-dismissible">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+        <h5><i class="icon fas fa-ban"></i> Gagal</h5>
+            Nama Lokasi Sama Sudah Ada
+    </div>
+<?php
+	} else {
+	$insertSql = "INSERT INTO lokasi SET nama_lokasi = ?";
+	$stmt 	   = $db->prepare($insertSql);
+	$stmt->bindParam(1, $_POST['nama_lokasi']);
+	if ($stmt->execute()){
+		$_SESSION['hasil'] = true;
+        $_SESSION['pesan'] = "Berhasil Simpan Data";
+	} else {
+		$_SESSION['hasil'] = true;
+        $_SESSION['pesan'] = "Gagal Simpan Data";
+	}
+    echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
+    }
+}
+?>
+
 <section class="content-header">
     <div class="container-fluid">
         <div class="row mb2">
@@ -35,5 +71,3 @@
         </div>
 </div>
 </section>
-
-<?php include_once "partials/scripts.php" ?>
